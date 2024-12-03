384b33bd72b6c60157bca4126b259504e375279d
AryanKumar-crypto <aryankumar110502@gmail.com>
2024-12-03 12:39:39 +0530

    program added

---
+++ dtaetimemodule.py
@@ -0,0 +1,18 @@
+import datetime
+x=datetime.datetime.now()
+a=x.strftime("%B")
+print(a)
+
+'''
+%b - month name , short version
+%B - month name , full version
+%m - month as a number 
+%y - year without century
+%Y - year eith century
+%H - hour (00-23)
+%I - hour (00-11)
+%p - AM/PM
+%M - minutes
+%S - Second
+           
+'''
